5a5885a6d46014c54f1a5ce2f8ccd06eeeeb7572
craig cardwell <wizeduvcc@gmail.com>
2022-05-17 18:35:46 -0600

    feat: merchants index with serializer and passing tests

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -5,7 +5,11 @@ describe "Merchants API" do
     create_list(:merchant, 3)
 
     get '/api/v1/merchants'
-
+require "pry"; binding.pry
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize: true)
+
+    expect(merchants.count).to eq(3)
   end
 end
