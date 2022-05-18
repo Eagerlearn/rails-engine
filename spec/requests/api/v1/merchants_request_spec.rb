1eaf15cecb44e51bf6c0179e7626ba29a382d07f
craig cardwell <wizeduvcc@gmail.com>
2022-05-17 18:40:10 -0600

    wip: merchant show spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -5,11 +5,37 @@ describe "Merchants API" do
     create_list(:merchant, 3)
 
     get '/api/v1/merchants'
-require "pry"; binding.pry
+
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body, symbolize: true)
+    response_body = JSON.parse(response.body, symbolize_names: true)
+
+    merchants = response_body[:data]
 
     expect(merchants.count).to eq(3)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_a(String)
+      expect(merchant[:type]).to eq('merchant')
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
+    end
+  end
+
+  xit "can get one merchant by its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+
+    merchant = response_body[:data]
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_a(String)
+    expect(merchant[:type]).to eq('merchant')
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(merchant[:attributes][:name]).to be_a(String)
   end
 end
