88da7113c3e9b900ed944cf2fc3e47a8e65e9897
craig cardwell <wizeduvcc@gmail.com>
2022-05-18 19:02:34 -0600

    request: add request route and controller action for endpoint response to return a single item by id

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/items', to: 'items#index'
+      get '/items/:id', to: 'items#show'
       get '/merchants', to: 'merchants#index'
       get '/merchants/:id', to: 'merchants#show'
       get '/merchants/:id/items', to: 'merchant_items#index', as: :specific_merchant_items
